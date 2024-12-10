dd7b23bc12d29c9084a92ecfc9a8f9860fa28264
Seokho <seokho0120@gmail.com>
2024-12-11 00:22:03 +0900

    feat: 버튼 추가, 스타일 추가, sass 추가

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,24 @@
 
 <template>
   <div class="pt-24">
-    <div class="flex justify-center max-w-[642px] m-auto">
-      <h1 class="text-5xl font-bold text-center">Minimal Carousel</h1>
+    <div class="flex flex-col justify-center items-center gap-4 max-w-[642px] m-auto">
+      <h1 class="text-5xl font-bold text-center text-[#181818]">Minimal Carousel</h1>
+      <p class="text-center text-lg text-[#181818]">
+        A user-friendly carousel library with essential features for quick implementation.
+      </p>
+      <div class="flex gap-2">
+        <button
+          class="bg-[#181818] hover:bg-[#282828] text-[#FCFCFC] text-xs px-8 font-semibold rounded-md w-[152px] h-[42px]"
+        >
+          Document
+        </button>
+
+        <button
+          class="bg-gradient-to-br from-white to-[#f0f0f0] text-black text-xs px-8 font-semibold rounded-md w-[152px] h-[42px] shadow-md border border-gray-200 hover:bg-gradient-to-br hover:from-[#f0f0f0] hover:to-white"
+        >
+          Github
+        </button>
+      </div>
     </div>
   </div>
 </template>
