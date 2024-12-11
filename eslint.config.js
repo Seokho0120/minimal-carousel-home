22c2ecfc0090232ec36d22e79214add5214d099d
Seokho <seokho0120@gmail.com>
2024-12-12 00:07:06 +0900

    feat: eslint 룰 추가, sonner 추가, 전체 스타일 수정, 텍스트 카피 기능 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
   {
@@ -16,4 +16,11 @@ export default [
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   skipFormatting,
-]
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off', // 다중 단어 이름 규칙 비활성화
+      '@typescript-eslint/no-unused-vars': 'off', // 사용되지 않는 변수 규칙 비활성화
+    },
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { toast, Toaster } from 'vue-sonner';
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(toast);
+app.component('Toaster', Toaster);
 
-app.mount('#app')
+app.mount('#app');
