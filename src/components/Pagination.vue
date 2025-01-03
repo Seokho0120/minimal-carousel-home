3df6c185ad6db3bc2196abae4520baf4b341f6ec
Seokho <seokho0120@gmail.com>
2025-01-04 01:52:11 +0900

    feat: 페이지네이션 추가(설명 수정 예정)

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,63 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { MinimalCarousel } from 'minimal-carousel';
+import DocIntro from './docItems/DocIntro.vue';
+import DocItem from './docItems/DocItem.vue';
+import DocItemType from './docItems/DocItemType.vue';
+import { IMAGES } from '@/constants/ImgData';
+
+const typeCode = `pagination: boolean;`;
+const exampleShortCode = `<MinimalCarousel pagination/>`;
+const exampleFullCode = `<script setup lang='ts'>
+import img1 from '../img1.jpg';
+import img2 from '../img2.jpg';
+import img3 from '../img3.jpg';
+
+const IMAGES = [
+  { link: 'img1.jpg', id: 1, name: 'img1' },
+  { link: 'img2.jpg', id: 2, name: 'img2' },
+  { link: 'img3.jpg', id: 3, name: 'img3' },
+];
+
+<\/script>
 
 <template>
-  <div>Pagination</div>
+  <MinimalCarousel :imageItems="IMAGES" pagination/>
+<\/template>
+`;
+</script>
+
+<template>
+  <div class="py-16">
+    <div class="max-w-[642px] w-full mx-auto">
+      <DocIntro title="Pagination" description="Pagination 설명" />
+
+      <div class="mt-12 w-full">
+        <DocItemType
+          title="Type of Pagination"
+          description="The type of Pagination is boolean, and the default value is true."
+          :shortCode="typeCode"
+        />
+      </div>
+
+      <div class="mt-12 w-full">
+        <DocItem
+          title="Example"
+          description="pagination 설명"
+          :shortCode="exampleShortCode"
+          :longCode="exampleFullCode"
+          example
+        >
+          <template #carousel>
+            <div class="mt-2 p-6 border-[1px] rounded-xl">
+              <MinimalCarousel
+                :imageItems="IMAGES"
+                class="rounded-lg"
+                pagination
+              />
+            </div>
+          </template>
+        </DocItem>
+      </div>
+    </div>
+  </div>
 </template>
